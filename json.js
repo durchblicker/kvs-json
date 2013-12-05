eecd27fa5f26034edd9713bd26f2e2755fa73c7b
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-12-05 19:44:16 +0100

    Initial Version

---
+++ json.js
@@ -0,0 +1,32 @@
+/*
+** © 2013 by Philipp Dunkel <pip@pipobscure.com>. Licensed under MIT-License.
+*/
+/*jshint node:true, browser:false*/
+'use strict';
+
+module.exports = KVSJson;
+var Abstract = require('kvs-abstract');
+
+Abstract.bequeath(KVSJson);
+function KVSJson(store) {
+  Abstract.instantiate(this);
+  this.store = store;
+  this._list = store.list.bind(store);
+  this._remove = store.remove.bind(store);
+}
+
+KVSJson.prototype._get = function(name, callback) {
+  this.store.get(name, function(err, val) {
+    if(err) return callback(err);
+    try {
+      val = JSON.parse(Buffer.isBuffer(val) ? val.toString('utf-8') : String(val));
+    } catch(ex) {
+      return callback(ex);
+    }
+    callback(null, val);
+  });
+};
+KVSJson.prototype._set = function(name, value, callback) {
+  value = Buffer.isBuffer(value) ? value : JSON.stringify(value);
+  this.store.set(name, value, callback);
+};
